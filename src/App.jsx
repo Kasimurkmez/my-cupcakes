103645ce5e5a18b03d530532c4801bc38fe89f22
kasimurkmez <kasimurkmez47@gmail.com>
2023-07-08 19:00:35 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { Link } from "wouter";
+
+import { cupcakes } from "./cupcakes.js";
+
 function App() {
   return (
-    <>
-      <div>
-        <h2 className="text-3xl text-red-800">HEllo</h2>
+    <div className="max-w-3xl m-auto py-10 px-8">
+      <h1 className="text-5xl font-bold mb-10">Cupcakes</h1>
+      <div className="grid grid-cols-2 gap-6">
+        {cupcakes.map((cupcake) => {
+          return (
+            <div key={cupcake.id} className="card bg-base-100 shadow-xl p-0">
+              <div>
+                <img src={cupcake.image} alt="" />
+              </div>
+              <div className="px-8 py-4 pb-6">
+                <h2 className="card-title mb-4 text-2xl font-bold">
+                  {cupcake.title}
+                </h2>
+                <div className="card-actions">
+                  <Link href={`/cupcakes/${cupcake.id}`}>
+                    <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                      Read More
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Link, Route } from "wouter";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import About from "./About.jsx";
+import Cupcake from "./Cupcake.jsx";
+
+import "./index.css";
+
+const Router = () => (
+  <div>
+    <div className="max-w-3xl m-auto py-2 px-8">
+      <div className="flex gap-4 text-lg">
+        <Link href="/">
+          <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+            Home
+          </a>
+        </Link>
+        <Link href="/about">
+          <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+            About
+          </a>
+        </Link>
+      </div>
+    </div>
+
+    <Route path="/" component={App} />
+    <Route path="/about" component={About} />
+    <Route path="/cupcakes/:id">{(params) => <Cupcake id={params.id} />}</Route>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router />
+  </React.StrictMode>
+);
